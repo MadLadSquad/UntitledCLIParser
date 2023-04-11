22c63e1ff988fc5769325e1e152ec20e0d832b62
Madman10K <stanv.contact176@gmail.com>
2023-04-11 21:35:14 +0300

    Updated comment documentation on event safety in the UntitledImGuiFramework

--- C/cucli.h
+++ C/cucli.h
@@ -6,16 +6,29 @@
 extern "C"
 {
 #endif
+    // UntitledImGuiFramework Event Safety - Any time
     void UCLI_Parser_init(struct UCLI_Parser_Data* data);
 
+
+    // UntitledImGuiFramework Event Safety - Any time
     void UCLI_Parser_setDelimiter(struct UCLI_Parser_Data* data, char del);
+
+    // UntitledImGuiFramework Event Safety - Any time
     void UCLI_Parser_setArgumentStyleWindows(struct UCLI_Parser_Data* data, bool bWindows);
 
+
+    // UntitledImGuiFramework Event Safety - Any time
     void UCLI_Parser_setUnknownArgumentCallback(struct UCLI_Parser_Data* data, UCLI_Parser_UnknownArgumentsCallback func, void* callbackData);
+
+    // UntitledImGuiFramework Event Safety - Any time
     void UCLI_Parser_setDefaultArray(struct UCLI_Parser_Data* data, void* additionalData, UCLI_Parser_ArrayFlagFunc func);
 
+
+    // UntitledImGuiFramework Event Safety - Any time
     void UCLI_Parser_setBooleanFlipping(struct UCLI_Parser_Data* data, bool bFlip);
 
+
+    // UntitledImGuiFramework Event Safety - Any time
     void UCLI_Parser_parse(struct UCLI_Parser_Data* data, int argc, char** argv,
                            struct UCLI_Parser_ArrayFlag* arrayFlags, size_t arrayFlagsSize,
                            struct UCLI_Parser_BooleanFlag* booleanFlags, size_t booleanFlagsSize,
@@ -23,6 +36,8 @@ extern "C"
                            struct UCLI_Parser_Pair* pairs, size_t pairsSize,
                            struct UCLI_Parser_PairWithFunc* pairsWithFunc, size_t pairsWithFuncSize);
 
+
+    // UntitledImGuiFramework Event Safety - Any time
     void UCLI_Parser_cleanupPairs(struct UCLI_Parser_Pair* pairs, size_t pairsSize);
 
 #ifdef __cplusplus
--- CLIParser.hpp
+++ CLIParser.hpp
@@ -5,22 +5,30 @@
 
 namespace UCLI
 {
+    // UntitledImGuiFramework Event Safety - Any time but recommended you use at Startup
     class UVK_PUBLIC_API Parser
     {
     public:
         Parser() noexcept;
 
+        // UntitledImGuiFramework Event Safety - Any time
         void init() noexcept;
 
+        // UntitledImGuiFramework Event Safety - Any time
         void setDelimiter(char del) noexcept;
+        // UntitledImGuiFramework Event Safety - Any time
         void setArgumentStyleWindows(bool bWindows) noexcept;
 
+        // UntitledImGuiFramework Event Safety - Any time
         void setBooleanFlipping(bool bFlip) noexcept;
 
+        // UntitledImGuiFramework Event Safety - Any time
         void setUnknownArgumentCallback(UCLI_Parser_UnknownArgumentsCallback func, void* callbackData = nullptr) noexcept;
 
+        // UntitledImGuiFramework Event Safety - Any time
         void setDefaultArray(void* additionalData, UCLI_Parser_ArrayFlagFunc func) noexcept;
 
+        // UntitledImGuiFramework Event Safety - Any time
         void parse(int argc, char** argv,
                    UCLI_Parser_ArrayFlag* arrayFlags = nullptr, size_t arrayFlagsSize = 0,
                    UCLI_Parser_BooleanFlag* booleanFlags = nullptr, size_t booleanFlagsSize = 0,
@@ -28,6 +36,7 @@ namespace UCLI
                    UCLI_Parser_Pair* pairs = nullptr, size_t pairsSize = 0,
                    UCLI_Parser_PairWithFunc* pairsWithFunc = nullptr, size_t pairsWithFuncSize = 0) noexcept;
 
+        // UntitledImGuiFramework Event Safety - Any time
         static void cleanupPairs(UCLI_Parser_Pair* pairs, size_t pairsSize);
     private:
         void parseShortArgument(std::vector<char*>& args, std::string& singleChar, std::u32string& singleCharU32, const std::string& tmp,
