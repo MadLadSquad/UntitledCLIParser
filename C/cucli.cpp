c466b808c4ce9ea1460447b93749e6b9dec47f6c
Madman10K <stanv.contact176@gmail.com>
2023-07-09 18:14:41 +0300

    Update to not require struct in C and to also export the C API on Windows

--- C/cucli.cpp
+++ C/cucli.cpp
@@ -6,36 +6,37 @@ extern "C"
 #endif
 #define to_class(data) ((UCLI::Parser*)data)
 
-    void UCLI_Parser_init(struct UCLI_Parser_Data* data)
+    void UCLI_Parser_init(UCLI_Parser_Data* data)
     {
         to_class(data)->init();
     }
 
-    void UCLI_Parser_setDelimiter(struct UCLI_Parser_Data* data, char del)
+    void UCLI_Parser_setDelimiter(UCLI_Parser_Data* data, char del)
     {
         to_class(data)->setDelimiter(del);
     }
-    void UCLI_Parser_setArgumentStyleWindows(struct UCLI_Parser_Data* data, bool bWindows)
+
+    void UCLI_Parser_setArgumentStyleWindows(UCLI_Parser_Data* data, bool bWindows)
     {
         to_class(data)->setArgumentStyleWindows(bWindows);
     }
 
-    void UCLI_Parser_setUnknownArgumentCallback(struct UCLI_Parser_Data* data, UCLI_Parser_UnknownArgumentsCallback func, void* callbackData)
+    void UCLI_Parser_setUnknownArgumentCallback(UCLI_Parser_Data* data, UCLI_Parser_UnknownArgumentsCallback func, void* callbackData)
     {
         to_class(data)->setUnknownArgumentCallback(func, callbackData);
     }
 
-    void UCLI_Parser_setDefaultArray(struct UCLI_Parser_Data* data, void* additionalData, UCLI_Parser_ArrayFlagFunc func)
+    void UCLI_Parser_setDefaultArray(UCLI_Parser_Data* data, void* additionalData, UCLI_Parser_ArrayFlagFunc func)
     {
         to_class(data)->setDefaultArray(additionalData, func);
     }
 
-    void UCLI_Parser_parse(struct UCLI_Parser_Data* data, int argc, char** argv,
-                           struct UCLI_Parser_ArrayFlag* arrayFlags, size_t arrayFlagsSize,
-                           struct UCLI_Parser_BooleanFlag* booleanFlags, size_t booleanFlagsSize,
-                           struct UCLI_Parser_BooleanFlagWithFunc* booleanFlagsWithFunc, size_t booleanFlagsWithFuncSize,
-                           struct UCLI_Parser_Pair* pairs, size_t pairsSize,
-                           struct UCLI_Parser_PairWithFunc* pairsWithFunc, size_t pairsWithFuncSize)
+    void UCLI_Parser_parse(UCLI_Parser_Data* data, int argc, char** argv,
+                           UCLI_Parser_ArrayFlag* arrayFlags, size_t arrayFlagsSize,
+                           UCLI_Parser_BooleanFlag* booleanFlags, size_t booleanFlagsSize,
+                           UCLI_Parser_BooleanFlagWithFunc* booleanFlagsWithFunc, size_t booleanFlagsWithFuncSize,
+                           UCLI_Parser_Pair* pairs, size_t pairsSize,
+                           UCLI_Parser_PairWithFunc* pairsWithFunc, size_t pairsWithFuncSize)
     {
         to_class(data)->parse(argc, argv,
                               arrayFlags, arrayFlagsSize,
@@ -45,12 +46,12 @@ extern "C"
                               pairsWithFunc, pairsWithFuncSize);
     }
 
-    void UCLI_Parser_cleanupPairs(struct UCLI_Parser_Pair* pairs, size_t pairsSize)
+    void UCLI_Parser_cleanupPairs(UCLI_Parser_Pair* pairs, size_t pairsSize)
     {
         UCLI::Parser::cleanupPairs(pairs, pairsSize);
     }
 
-    void UCLI_Parser_setBooleanFlipping(struct UCLI_Parser_Data* data, bool bFlip)
+    void UCLI_Parser_setBooleanFlipping(UCLI_Parser_Data* data, bool bFlip)
     {
         to_class(data)->setBooleanFlipping(bFlip);
     }
--- C/cucli.h
+++ C/cucli.h
@@ -7,38 +7,38 @@ extern "C"
 {
 #endif
     // UntitledImGuiFramework Event Safety - Any time
-    void UCLI_Parser_init(struct UCLI_Parser_Data* data);
+    UVK_PUBLIC_API void UCLI_Parser_init(UCLI_Parser_Data* data);
 
 
     // UntitledImGuiFramework Event Safety - Any time
-    void UCLI_Parser_setDelimiter(struct UCLI_Parser_Data* data, char del);
+    UVK_PUBLIC_API void UCLI_Parser_setDelimiter(UCLI_Parser_Data* data, char del);
 
     // UntitledImGuiFramework Event Safety - Any time
-    void UCLI_Parser_setArgumentStyleWindows(struct UCLI_Parser_Data* data, bool bWindows);
-
-
-    // UntitledImGuiFramework Event Safety - Any time
-    void UCLI_Parser_setUnknownArgumentCallback(struct UCLI_Parser_Data* data, UCLI_Parser_UnknownArgumentsCallback func, void* callbackData);
-
-    // UntitledImGuiFramework Event Safety - Any time
-    void UCLI_Parser_setDefaultArray(struct UCLI_Parser_Data* data, void* additionalData, UCLI_Parser_ArrayFlagFunc func);
+    UVK_PUBLIC_API void UCLI_Parser_setArgumentStyleWindows(UCLI_Parser_Data* data, bool bWindows);
 
 
     // UntitledImGuiFramework Event Safety - Any time
-    void UCLI_Parser_setBooleanFlipping(struct UCLI_Parser_Data* data, bool bFlip);
+    UVK_PUBLIC_API void UCLI_Parser_setUnknownArgumentCallback(UCLI_Parser_Data* data, UCLI_Parser_UnknownArgumentsCallback func, void* callbackData);
+
+    // UntitledImGuiFramework Event Safety - Any time
+    UVK_PUBLIC_API void UCLI_Parser_setDefaultArray(UCLI_Parser_Data* data, void* additionalData, UCLI_Parser_ArrayFlagFunc func);
 
 
     // UntitledImGuiFramework Event Safety - Any time
-    void UCLI_Parser_parse(struct UCLI_Parser_Data* data, int argc, char** argv,
-                           struct UCLI_Parser_ArrayFlag* arrayFlags, size_t arrayFlagsSize,
-                           struct UCLI_Parser_BooleanFlag* booleanFlags, size_t booleanFlagsSize,
-                           struct UCLI_Parser_BooleanFlagWithFunc* booleanFlagsWithFunc, size_t booleanFlagsWithFuncSize,
-                           struct UCLI_Parser_Pair* pairs, size_t pairsSize,
-                           struct UCLI_Parser_PairWithFunc* pairsWithFunc, size_t pairsWithFuncSize);
+    UVK_PUBLIC_API void UCLI_Parser_setBooleanFlipping(UCLI_Parser_Data* data, bool bFlip);
 
 
     // UntitledImGuiFramework Event Safety - Any time
-    void UCLI_Parser_cleanupPairs(struct UCLI_Parser_Pair* pairs, size_t pairsSize);
+    UVK_PUBLIC_API void UCLI_Parser_parse(UCLI_Parser_Data* data, int argc, char** argv,
+                           UCLI_Parser_ArrayFlag* arrayFlags, size_t arrayFlagsSize,
+                           UCLI_Parser_BooleanFlag* booleanFlags, size_t booleanFlagsSize,
+                           UCLI_Parser_BooleanFlagWithFunc* booleanFlagsWithFunc, size_t booleanFlagsWithFuncSize,
+                           UCLI_Parser_Pair* pairs, size_t pairsSize,
+                           UCLI_Parser_PairWithFunc* pairsWithFunc, size_t pairsWithFuncSize);
+
+
+    // UntitledImGuiFramework Event Safety - Any time
+    UVK_PUBLIC_API void UCLI_Parser_cleanupPairs(UCLI_Parser_Pair* pairs, size_t pairsSize);
 
 #ifdef __cplusplus
 }
